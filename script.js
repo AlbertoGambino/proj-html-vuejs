af92a86565003ed73e881ad5fe0aac4b348c9da6
Alberto Gambino <gambafb@libero.it>
2021-04-29 13:31:49 +0100

    Vue + transizioni layer

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('hello');
+
+function hfVue (){
+
+  new Vue({
+
+        el: '#app',
+        data:{
+          headers: [
+            'HOME',
+            'ABOUT US',
+            'FACILITIES',
+            'MEMBERSHIP',
+            'TESTIMONIALS'],
+          footers: [
+            '© Copyright 2012 - 2021',
+            'Avada Theme by Theme Fusion',
+            'All Right reserved',
+            'Powered by WordPress'
+          ]
+
+        }
+
+  })
+
+
+}
+
+
+function init() {
+
+    console.log('hello world');
+
+    hfVue()
+}
+$(init);
